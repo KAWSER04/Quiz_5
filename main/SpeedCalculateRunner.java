420bb50ae187db1d56a5bae8eb33c74837589e2d
kawser <kawser.cse.ru@gmail.com>
2018-11-25 21:46:57 +0600

    speed test

---
+++ main/SpeedCalculateRunner.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class SpeedCalculateRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter the of Mass: ");
+		Scanner in=new Scanner(System.in);
+		double m=in.nextDouble();
+		SpeedCalculate obj=new SpeedCalculate();
+		obj.setMass(m);
+		int speed=obj.calculateSpeed();
+		System.out.println("The maximum speed is:"+speed);
+
+	}
+
+}
